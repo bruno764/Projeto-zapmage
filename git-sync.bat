71158ed135dd9c54aa0c871ac62e73e2192d0654
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-29 16:19:16 -0300

    update: sincroniza├º├úo autom├ítica com altera├º├Áes locais

---
+++ git-sync.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Users\bruno\OneDrive\Área de Trabalho\Projeto Magezap"
+git add .
+git commit -m "update: sincronização automática com alterações locais"
+git push origin main
+pause
