f19885d75710969527911abe98e2cc05378fd08e
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-30 22:27:51 -0300

    update: sincroniza├º├úo autom├ítica com altera├º├Áes locais

--- codatendechat-main/frontend/src/index.js
+++ codatendechat-main/frontend/src/index.js
@@ -4,21 +4,12 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 
 import App from "./App";
 
-import { AuthProvider } from "./context/Auth/AuthContext";
-import { CampaignsProvider } from "./context/Campaigns/CampaignsContext";
-
 ReactDOM.render(
-	<React.StrictMode>
-	  <CssBaseline />
-	  <AuthProvider>
-		<CampaignsProvider>
-		  {/* qualquer outro provider que precisar */}
-		  <App />
-		</CampaignsProvider>
-	  </AuthProvider>
-	</React.StrictMode>,
+	<CssBaseline>
+		<App />
+	</CssBaseline>,
 	document.getElementById("root")
-  );
+);
 
 // ReactDOM.render(
 // 	<React.StrictMode>
