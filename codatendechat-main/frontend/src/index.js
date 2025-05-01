ac9bda1d88fb72c6c69b1367d227ae1348784d1c
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-30 22:42:14 -0300

    update: sincroniza├º├úo autom├ítica com altera├º├Áes locais

--- codatendechat-main/frontend/src/index.js
+++ codatendechat-main/frontend/src/index.js
@@ -1,22 +1,20 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/Auth/AuthContext";
 
 ReactDOM.render(
-	<CssBaseline>
-		<App />
-	</CssBaseline>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <CssBaseline />
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// ReactDOM.render(
-// 	<React.StrictMode>
-// 		<CssBaseline>
-// 			<App />
-// 		</CssBaseline>,
-//   </React.StrictMode>
-
-// 	document.getElementById("root")
-// );
