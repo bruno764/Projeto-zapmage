6488fa2cdc9c5e6dc647dffee67e7cf89b920ef3
bruno764 <brunonog76418@gmail.com>
2025-05-03 16:32:53 -0300

    update: sincroniza├º├úo autom├ítica com altera├º├Áes locais

--- git-sync.bat
+++ git-sync.bat
@@ -1,5 +1,5 @@
 @echo off
-cd "C:\Users\bruno\OneDrive\Área de Trabalho\Projeto Magezap"
+cd "D:\Projeto-zapmage"
 git add .
 git commit -m "update: sincronização automática com alterações locais"
 git push origin main
